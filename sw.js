687b6c459ff0892b60127a3c1dc247af50d533d1
funtasticgamer101-collab <funtasticgamer101@gmail.com>
2026-03-28 15:15:09 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,41 +3,23 @@ const ASSETS = [
     './',
     './index.html',
     './style.css',
-    './game.js',
+    './script.js',
     './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
+    './icon.svg'
 ];
 
-// Install Service Worker
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Caching assets');
-                return cache.addAll(ASSETS);
-            })
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
     );
 });
 
-// Fetch from Cache or Network
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-// Update Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-            );
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
